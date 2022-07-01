d08f8c603b206c7ed8a3739dc8819c61eb402253
Igor <sergienko339@gmail.com>
2022-07-01 13:48:55 +0300

    update styles.ts

--- styles/styles.ts
+++ styles/styles.ts
@@ -1,29 +1,13 @@
 import styled from 'styled-components';
 
-export const Wrapp = styled.div`
-	display: flex;
-	flex-direction: ${({ fd }) => fd || `column`};
-	${({ jc }) => jc && `justify-content:${jc}`};
-	${({ ai }) => ai && `align-items:${ai}`};
-
-	${({ p }) => p && `padding: ${p}`};
-	${({ m }) => m && `margin: ${m}`};
-
-	${({ h }) => h && `height: ${h}`};
-	${({ minH }) => minH && `min-height: ${minH}`};
-	${({ maxH }) => maxH && `max-height: ${maxH}`};
-
-	${({ bg }) => bg && `background: ${bg}`};
-`;
-
-export const Row = styled.div`
+export const FlexBlock = styled.div`
 	${({ pos }) => pos && `position: ${pos}`};
 	${({ pos_T }) => pos_T && `top: ${pos_T}`};
 	${({ pos_L }) => pos_L && `left: ${pos_L}`};
 	${({ pos_R }) => pos_R && `right: ${pos_R}`};
 	${({ pos_B }) => pos_B && `bottom: ${pos_B}`};
 
-	display: flex;
+	display: ${({ d }) => d || `flex`};
 	flex-direction: ${({ fd }) => fd || `row`};
 	${({ jc }) => jc && `justify-content:${jc}`};
 	${({ ai }) => ai && `align-items:${ai}`};
@@ -42,28 +26,12 @@ export const Row = styled.div`
 	${({ bg }) => bg && `background: ${bg}`};
 `;
 
-export const Col = styled.div`
-	${({ pos }) => pos && `position: ${pos}`};
-	${({ pos_T }) => pos_T && `top: ${pos_T}`};
-	${({ pos_L }) => pos_L && `left: ${pos_L}`};
-	${({ pos_R }) => pos_R && `right: ${pos_R}`};
-	${({ pos_B }) => pos_B && `bottom: ${pos_B}`};
-
-	display: flex;
+export const Wrapp = styled(FlexBlock)`
 	flex-direction: ${({ fd }) => fd || `column`};
-	${({ jc }) => jc && `justify-content:${jc}`};
-	${({ ai }) => ai && `align-items:${ai}`};
+`;
 
-	${({ p }) => p && `padding: ${p}`};
-	${({ m }) => m && `margin: ${m}`};
+export const Row = styled(FlexBlock)``;
 
-	${({ w }) => w && `width: ${w}`};
-	${({ minW }) => minW && `min-width: ${minW}`};
-	${({ maxW }) => maxW && `max-width: ${maxW}`};
-
-	${({ h }) => h && `height: ${h}`};
-	${({ minH }) => minH && `min-height: ${minH}`};
-	${({ maxH }) => maxH && `max-height: ${maxH}`};
-
-	${({ bg }) => bg && `background: ${bg}`};
+export const Col = styled(FlexBlock)`
+	flex-direction: ${({ fd }) => fd || `column`};
 `;
